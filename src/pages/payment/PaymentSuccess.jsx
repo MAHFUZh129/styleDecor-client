0da9ae468c1ddb795e29324ed95017c441b05fde
mahfuzh129 <mahfuzh266@gmail.com>
2025-12-17 20:00:18 +0600

    added a new modal

--- src/pages/payment/PaymentSuccess.jsx
+++ src/pages/payment/PaymentSuccess.jsx
@@ -13,20 +13,21 @@ const PaymentSuccess = () => {
     }
   }, [sessionId])
   return (
-    <div className='flex flex-col items-center justify-center'>
-      <div className='bg-white p-10 rounded-lg shadow-lg text-center'>
-        <IoBagCheckOutline className='w-16 h-16 text-green-500 mx-auto mb-4' />
-        <h1 className='text-3xl font-bold text-gray-800 mb-2'>
-          Payment Successful!
+    <div className='flex flex-col pt-12 items-center justify-center'>
+      <div className='bg-secondary/10 p-10 rounded-lg shadow-lg text-center'>
+        <IoBagCheckOutline className='w-16 h-16 text-primary mx-auto mb-4' />
+        <h1 className='text-3xl font-bold italic text-gray-800 mb-2'>
+          Payment Successful !!
         </h1>
         <p className='text-gray-600 mb-6'>
-          Thank you for your purchase. Your order is being processed.
+          Thank you for your bookings. Your bookings being processed.
         </p>
         <Link
-        //   to='/dashboard/my-orders'
-          className='inline-block bg-lime-500 text-white font-semibold py-2 px-4 rounded hover:bg-lime-600 transition duration-300'
+        to="my-bookings"
+          
+          className='inline-block bg-gradient-to-r from-primary to-secondary text-white font-semibold py-2 px-4 rounded hover:bg-lime-600 transition duration-300'
         >
-          Go to My Orders
+          Go to My Bookings
         </Link>
       </div>
     </div>
--- src/pages/user/MyBookings.jsx
+++ src/pages/user/MyBookings.jsx
@@ -1,11 +1,88 @@
-import React from 'react';
+import { useQuery } from '@tanstack/react-query'
+import useAuth from '../../hooks/useAuth'
+import useAxiosSecure from '../../hooks/useAxiosSecure'
+import LoadingSpinner from '../../components/shared/LoadingSpinner'
+import BookingTable from '../../components/Table/BookingTable'
 
 const MyBookings = () => {
-    return (
-        <div>
-           MyBookings
-        </div>
-    );
-};
+    const { user } = useAuth()
+    const axiosSecure = useAxiosSecure()
+    const { data: bookings = [], isLoading } = useQuery({
+        queryKey: ['bookings', user?.email],
+        queryFn: async () => {
+            const result = await axiosSecure(`/my-bookings`)
+            return result.data
+        },
+    })
+    // console.log(bookings)
 
-export default MyBookings;
+    if (isLoading) return <LoadingSpinner />
+    return (
+        <>
+            <div className='container mx-auto px-4 sm:px-8'>
+                <div className='py-8'>
+                    <div className='-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto'>
+                        <div className='inline-block min-w-full shadow rounded-lg overflow-hidden'>
+                            <table className='min-w-full leading-normal'>
+                                <thead>
+                                    <tr>
+                                        <th
+                                            scope='col'
+                                            className='px-5 py-3 bg-white  border-b border-gray-200 text-primary  text-left text-sm uppercase font-semibold'
+                                        >
+                                            Image
+                                        </th>
+                                        <th
+                                            scope='col'
+                                            className='px-5 py-3 bg-white  border-b border-gray-200 text-primary  text-left text-sm uppercase font-semibold'
+                                        >
+                                            Name
+                                        </th>
+                                        <th
+                                            scope='col'
+                                            className='px-5 py-3 bg-white  border-b border-gray-200 text-primary  text-left text-sm uppercase font-semibold'
+                                        >
+                                            Category
+                                        </th>
+                                        <th
+                                            scope='col'
+                                            className='px-5 py-3 bg-white  border-b border-gray-200 text-primary  text-left text-sm uppercase font-semibold'
+                                        >
+                                            Price
+                                        </th>
+                                        <th
+                                            scope='col'
+                                            className='px-5 py-3 bg-white  border-b border-gray-200 text-primary  text-left text-sm uppercase font-semibold'
+                                        >
+                                            Quantity
+                                        </th>
+                                        <th
+                                            scope='col'
+                                            className='px-5 py-3 bg-white  border-b border-gray-200 text-primary  text-left text-sm uppercase font-semibold'
+                                        >
+                                            Status
+                                        </th>
+
+                                        <th
+                                            scope='col'
+                                            className='px-5 py-3 bg-white  border-b border-gray-200 text-primary  text-left text-sm uppercase font-semibold'
+                                        >
+                                            Action
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {bookings.map(booking => (
+                                        <BookingTable key={booking._id} booking={booking} />
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default MyBookings
